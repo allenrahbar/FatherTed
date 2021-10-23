eaf865c5db31791b5ccc6be2ff41ec846e1d6b69
allenrahbar <allen.rahbar@screencloud.io>
2021-10-23 17:26:31 +0000

    create function to calculate and store high scores

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,20 +4,31 @@ const saveScoreBtn = document.querySelector('#save-score-button');
 const finalScore = document.querySelector('#final-score');
 const mostRecentScore = document.querySelector('#most-recent-score');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 5;
 
 // Function so that whenever you press a key it will enable save button
 finalScore.innerText = mostRecentScore
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
-})
+});
 // This allows us to clear so it doesn't automatically refresh
+// Function to calculate high score
 saveHighScore = x => {
-    x.preventDefault()
+    x.preventDefault();
 
     const score = {
-        score: mostRecentScore
+        score: mostRecentScore,
+        name: username.value
     }
+    highScores.push(score)
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+highScores.splice(5)
+
+localStorage.setItem('highScores', JSON.stringify(highScores))
+window.location.assign('/')
+
 }
