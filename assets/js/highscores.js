97d66b82d1059e55a5407669c33c8caae4083e9a
allenrahbar <allen.rahbar@screencloud.io>
2021-10-24 17:37:15 +0000

    add js function to create list for players highscores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,3 +1,9 @@
-const highScoresList = document.querySelector('#high-scores-list')
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
-const highScore = JSON.parse(localStorage.getItem('highScores')) || []
+const highScoresList = document.querySelector('#high-scores-list');
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse;
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML = 
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+
+}).join('')
